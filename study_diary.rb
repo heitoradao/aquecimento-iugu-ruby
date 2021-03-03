c1790890fecb155bdfd7fd0864e203b7528905d5
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-03 19:34:32 -0300

    substituído números mágicos por constantes

--- study_diary.rb
+++ study_diary.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 require_relative 'study_item'
+
+REGISTER = 1
+VIEW     = 2
+SEARCH   = 3
+EXIT     = 4
 
 def clear
   system('clear')
@@ -20,10 +27,10 @@ def welcome
 end
 
 def menu
-  puts "[1] Cadastrar um item para estudar"
-  puts "[2] Ver todos os itens cadastrados"
-  puts "[3] Buscar um item de estudo"
-  puts "[4] Sair"
+  puts "[#{REGISTER}] Cadastrar um item para estudar"
+  puts "[#{VIEW}] Ver todos os itens cadastrados"
+  puts "[#{SEARCH}] Buscar um item de estudo"
+  puts "[#{EXIT}] Sair"
   print 'Escolha uma opção: '
   gets.to_i
 end
@@ -41,7 +48,7 @@ def print_items(collection)
   collection.each_with_index do |item, index|
     puts "##{index + 1} - #{item.title} - #{item.category}"
   end
-  puts 'Nenhum item cadastrado' if collection.empty?
+  puts 'Nenhum item encontrado' if collection.empty?
 end
 
 def search_items(collection)
@@ -51,7 +58,6 @@ def search_items(collection)
     item.title.include?(term)
   end
   print_items(found_items)
-  puts 'Nenhum item encontrado' if collection.empty?
 end
 
 clear
@@ -62,13 +68,13 @@ option = menu
 loop do
   clear
   case option
-  when 1
+  when REGISTER
     study_items << register_study_item
-  when 2
+  when VIEW
     print_items(study_items)
-  when 3
+  when SEARCH
     search_items(study_items)
-  when 4
+  when EXIT
     clear
     puts 'Obrigado por usar o Diário de Estudos'
     break
