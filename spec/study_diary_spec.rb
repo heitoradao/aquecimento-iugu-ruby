c4c5027364b8c5ce8d649c2032e1717eac7cc40a
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-05 02:58:39 -0300

    adicionados mais templates de teste para o diário

--- spec/study_diary_spec.rb
+++ spec/study_diary_spec.rb
@@ -7,22 +7,29 @@ describe StudyDiary do
     expect(study_diary).to be_instance_of(StudyDiary)
   end
 
-  #it 'MUST HAVE means to insert new study tracking information' do
-  #  expect(false).to eq(true)
-  #end
+  it 'MUST HAVE means to insert new study tracking information' do
+    diary = StudyDiary.new
 
-  #it 'MUST HAVE means to recover information about study track' do
-  #  open3
-  #  expect(false).to eq(true)
-  #end
+    guard = StudyItem(title: 'Guard Clause', category: 'Ruby')
+    diary.register(guard)
+
+    expect(false).to eq(true)
+  end
+
+  it 'MUST HAVE means to recover information about study track' do
+    #  open3
+    expect(false).to eq(true)
+  end
 
   it 'SHOULD be able to persist study information between sessions' do
     expect(false).to eq(true)
   end
 
-  it 'SHOULD work in non-interactive mode' do
-    expect(false).to eq(true)
-  end
+  #it 'SHOULD work in non-interactive mode' do
+    # by doing at least insertion, retrieval, completion
+    # would be nice if could export to json
+  #  expect(false).to eq(true)
+  #end
 
   it 'SHOULD be able to export study info in JSON format' do
     expect(false).to eq(true)
